90b67972d348a491f281455fae5cc3058beeeb1e
durmusmehmet <durmus_mehmet@msn.com>
2014-09-26 15:39:28 +0300

    Başlangıc Onayı

---
+++ WebTestProjesi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebTestProjesi.Startup))]
+namespace WebTestProjesi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
